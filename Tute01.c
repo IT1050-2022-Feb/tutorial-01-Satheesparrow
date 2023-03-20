15f7b3a69bf04da13a5cb9b65042400a79638f3c
Sathee Hettiarachchi <104905636+Satheesparrow@users.noreply.github.com>
2023-03-20 08:52:00 +0000

    test

--- Tute01.c
+++ Tute01.c
@@ -1,11 +1,12 @@
 /* Exercise 1 - Calculations
 
-   Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
+   Write a C program to input marks of two subjects. Calculate and print the
+   average of the two marks. */
 
 #include <stdio.h>
 
 int main() {
-//test
+  printf("Hello World !");
+
   return 0;
 }
-
